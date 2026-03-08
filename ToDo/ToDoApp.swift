fd06eaf41dde2a77512c73d9b305b73ab0444895
Reshid-505 <hreshid2016@gmail.com>
2026-03-08 13:26:27 +0400

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Reshid Huseynov on 08.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
