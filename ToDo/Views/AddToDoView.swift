16e2e8dbc77c308357bcf08255e990c61189a933
Reshid-505 <hreshid2016@gmail.com>
2026-03-10 18:43:56 +0400

    To Do App v1 ready

---
+++ ToDo/Views/AddToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  AddToDoView.swift
+//  ToDo
+//
+//  Created by Reshid Huseynov on 08.03.2026.
+//
+
+import SwiftUI
+
+struct AddToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddToDoView()
+}
---
+++ ToDo/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  ToDo
+//
+//  Created by Reshid Huseynov on 08.03.2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
